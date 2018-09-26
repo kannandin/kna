07532a6807bf6959cf4a4532d3bef9727a921c41
kannandin <37356111+kannandin@users.noreply.github.com>
2018-09-26 22:54:06 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a<0)
+    {
+        printf("Negative");
+    }
+        if(a>0)
+    {
+        printf("Positive");
+    }
+        if(a==0)
+    {
+        printf("Zero");
+    }
+    
+}
